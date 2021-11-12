d590e66fff1ea7cb150cbedcc259240f368e60c5
johnwalley <john@walley.org.uk>
2021-11-12 13:21:34 +0000

    chore: wip

---
+++ website/docs/intro.md
@@ -0,0 +1,23 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Let's discover **Allotment in less than 5 minutes**.
+
+## Getting Started
+
+Create React App is an officially supported way to create single-page React applications. It offers a modern build setup with no configuration. We'll use it to create an application to which we'll add Allotment.
+
+## Create a new application
+
+```bash
+npx create-react-app my-app
+cd my-app
+npm start
+```
+
+## Start your site
+
+Then open `http://localhost:3000/` to see your app.
---
+++ website/docusaurus.config.js
@@ -0,0 +1,95 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Allotment",
+  tagline: "React split-pane component",
+  url: "https://allotment.mulberryhousesoftware.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "johnwalley", // Usually your GitHub org/user name.
+  projectName: "allotment", // Usually your repo name.
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          remarkPlugins: [
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
+          ],
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/johnwalley/allotment/edit/main/website/",
+        },
+        pages: {
+          remarkPlugins: [require("@docusaurus/remark-plugin-npm2yarn")],
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Allotment",
+        logo: {
+          alt: "Allotment Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            href: "https://github.com/johnwalley/allotment",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/johnwalley/allotment",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} John Walley. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
