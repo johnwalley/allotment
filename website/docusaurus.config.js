16ec898731902ccaa9276267dc5f34c78d4e6da5
johnwalley <john@walley.org.uk>
2022-01-16 11:27:22 +0000

    fix: footer link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -68,8 +68,8 @@ const config = {
             title: "Docs",
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: "Getting started",
+                to: "/docs/getting-started",
               },
             ],
           },
