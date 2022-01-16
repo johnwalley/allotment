0a83571ec7fc3745a5be91f945c1e92f6bc38157
johnwalley <john@walley.org.uk>
2022-01-16 21:10:36 +0000

    feat: dark mode

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,34 @@
+import { themes } from "@storybook/theming";
+import { useDarkMode } from "storybook-dark-mode";
+
+export const decorators = [
+  (Story) => {
+    if (useDarkMode()) {
+      document.documentElement.style.setProperty(
+        "--card-background-color",
+        "rgb(30,30,30)"
+      );
+    } else {
+      document.documentElement.style.setProperty(
+        "--card-background-color",
+        "rgb(255,255,255)"
+      );
+    }
+
+    return <Story />;
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  backgrounds: {
-    default: "light",
-  },
+  backgrounds: { disable: true },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  darkMode: {
+    stylePreview: true,
   },
 };
