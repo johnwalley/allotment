80bc6bd3c2f65ed417cd8bd209bef3f74fc97214
John Walley <john@walley.org.uk>
2022-01-22 19:49:18 +0000

    docs: add website (#110)

---
+++ website/docs/overview.md
@@ -0,0 +1,8 @@
+---
+id: overview
+title: Overview
+---
+
+# API Overview
+
+For most purposes the `Allotment` component is all that is needed. Consider using the `Allotment.Pane` sub-component if you need more fine-grained control over the individual panes.
---
+++ website/docusaurus.config.js
@@ -0,0 +1,94 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Allotment",
+  tagline: "React split-pane component",
+  url: "https://allotment.mulberryhousesoftware.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "johnwalley", // Usually your GitHub org/user name.
+  projectName: "allotment", // Usually your repo name.
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          remarkPlugins: [
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
+          ],
+          sidebarPath: require.resolve("./sidebars.json"),
+        },
+        pages: {
+          remarkPlugins: [require("@docusaurus/remark-plugin-npm2yarn")],
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Allotment",
+        logo: {
+          alt: "Allotment Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "getting-started",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            href: "https://github.com/johnwalley/allotment",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Getting started",
+                to: "/docs/getting-started",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/johnwalley/allotment",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} John Walley. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
