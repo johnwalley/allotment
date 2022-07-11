d0e1ecc5f69c669d5fe5759632a462d2b417bd4d
johnwalley <john@walley.org.uk>
2022-07-11 09:32:36 +0100

    fix: export LayoutPriority enum

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export type { AllotmentHandle, AllotmentProps } from "./allotment";
 export { default as Allotment, setSashSize } from "./allotment";
+export type { LayoutPriority } from "./split-view";
