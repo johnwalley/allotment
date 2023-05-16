616eb97c45ccbc369222960e2576b6c65dbbce45
Su-Yong <simssy2205@gmail.com>
2023-05-16 05:23:16 -0700

    fix: fix storybook, rollup build error

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -20,7 +20,7 @@ const config = {
   external: [/@babel\/runtime/, ...Object.keys(globals)],
   output: [
     {
-      file: meta.exports["."],
+      file: meta.exports["."].main,
       format: "es",
       plugins: [
         getBabelOutputPlugin({
