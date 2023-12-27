3fb3e61f531f63b72f2d9973800b28ada7015986
johnwalley <john@walley.org.uk>
2023-12-27 23:59:08 +0000

    chore: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,26 @@
 const path = require("path");
 
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-dark-mode",
   ],
+
   features: {
     postcss: false,
   },
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
+
   webpackFinal: async (baseConfig, options) => {
     // Modify or replace config. Mutating the original reference object can cause unexpected bugs.
     const { module = {} } = baseConfig;
@@ -39,7 +40,7 @@ module.exports = {
 
     // First we prevent webpack from using Storybook CSS rules to process CSS modules
     newConfig.module.rules.find(
-      (rule) => rule.test.toString() === "/\\.css$/"
+      (rule) => rule.test.toString() === "/\\.css$/",
     ).exclude = /\.module\.css$/;
 
     // Then we tell webpack what to do with CSS modules
@@ -62,5 +63,9 @@ module.exports = {
     });
 
     return newConfig;
+  },
+
+  docs: {
+    autodocs: false,
   },
 };
