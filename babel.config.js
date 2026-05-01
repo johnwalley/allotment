95fc9f6532cbf1e9879f1b4a6b4440cd4a6ea355
John Walley <john@walley.org.uk>
2026-05-01 13:20:55 +0100

    chore(deps): replace deprecated Babel proposal plugins with transform equivalents

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
   ],
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
